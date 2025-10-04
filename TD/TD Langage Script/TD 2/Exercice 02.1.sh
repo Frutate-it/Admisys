1ba571c76520a4ce824e15f9a63c80d6f679407a
Frutate <dumur@hotmail.fr>
2025-10-04 15:45:46 +0200

    .

--- TD/TD Langage Script/TD 2/Exercice 02.1.sh
+++ TD/TD Langage Script/TD 2/Exercice 02.1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#commande pour afficher le nombre de fichiers ayant une permission spécial
+#Commande pour afficher le nombre de fichiers ayant une permission spécial
 #On affiche d'abord le contenu avec ls, on récupère l'octet qui nous intéresse avec cut soit le 10ème
 #On filtre et on compte les entrée correspondant avec grep et l'option -i pour la casse
 
